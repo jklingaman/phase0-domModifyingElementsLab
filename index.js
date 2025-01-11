66eaa32217bf5578b57b213b9c99136e6bb00abf
Jackson Klingaman <jackson.klingaman22@gmail.com>
2025-01-10 18:51:53 -0600

    passes all tests

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+const yourName = "Jackson";
+newHeader.textContent = `${yourName} is the champion`;
